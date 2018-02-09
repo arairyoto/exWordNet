178fb457d7c40e08322625fe0e7b330a525f6e83
arairyoto <arai.ryoto@gmail.com>
2018-02-09 19:30:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,190 @@
+# exWordNet
+
+## Description
+WordNetの拡張です．基本的なWordNetの機能は使えます．
+
+datafile：https://www.dropbox.com/sh/ckylez7t5x3ecm5/AAA7yizJ5CA3mF8nE5Teo2ARa?dl=0
+
+datafileのfolderのパスをexWordNetの定義の時に指定してください．
+
+
+**WordNetとの相違点（追加点）**
+- Wordオブジェクトの追加
+
+  Synset, Lemmaに加え，Wordオブジェクトを追加しました．
+
+- ベクトルプロパティの追加
+
+  Synset, Lemma, Wordそれぞれのオブジェクトにベクトルプロパティを追加しました．
+
+- 頻度プロパティの追加
+
+  Lemmaオブジェクトにはトピックごとの頻度情報を追加しました．
+
+- トピックベクトルの追加
+
+  トピックごとのトピックベクトルを追加しました．
+
+### Word Object
+Word Objectに定義されているプロパティとメソッドに関して説明します．
+Word Objectは語・品詞・言語で特定します．
+
+**Property**
+
+- name
+
+  語(e.g. "design")
+
+- pos
+
+  品詞(e.g. "n")
+
+- lang
+
+  言語(e.g. "eng")/WordNetでの言語名と同一
+
+- synsets
+
+  語と紐付くSynsetのリスト
+
+- lemmas
+
+  語と紐付くLemmaのリスト
+
+**Method**
+
+- vector(topic=None)
+
+  語のトピックごとのベクトル表現を返します．topicに何も指定しないと，ジェネラルな語のベクトル表現を返します．
+
+- ambiguity(topic='general')
+
+  トピックごとの曖昧性を返します．topicに何も指定しないと，WordNetにもともと実装されているSemCor Corpusを元にした頻度情報による曖昧性の計算をします．
+
+- topic relatedness(topic)
+
+  トピックとの関連性を返します．トピックを必ず指定する必要があります．
+
+- association(other, topic)
+
+  トピックごとの当該Word Object(other)との連想関連指標を返します．topicに何も指定しないとジェネラルな語のベクトル表現で算出します．
+
+### New Method for exWordNet
+exWordNetに新たに追加したメソッドについて説明します．
+
+- vector(obj)
+
+  引数のObjectのベクトル表現を返します．ObjectとしてはSynset, Lemma, Word Objectが可能です．
+
+- lemma_freq(lemma, topic)
+
+  引数のlemmaの当該topic下での頻度情報を返します．頻度情報は，全体のlemmaの出現頻度数で割られており，正規化されています．
+
+- topic_vector(topic)
+
+  引数のtopicのトピックベクトル表現を返します．
+
+- relatedness(obj_in, obj_out)
+
+  引数のObject同士のベクトル表現によるコサイン類似度を算出します．
+
+## How to Use
+
+```
+from exWordNet import exWordNet
+
+exwn = exWordNet([root of data folder])
+
+########################################
+# same as normal wordnet
+########################################
+
+# definition of synset
+synset = exwn.synset('design.n.01')
+synsets = exwn.synsets('design')
+
+# definition of lemma
+lemma = exwn.lemmas('design')
+
+# iterator
+for synset in exwn.all_synsets(pos=[pos], lang=[lang]):
+  ...
+
+########################################
+# new feature Word Object
+########################################
+
+# definition of word
+exwn.word('design', 'n')
+-> Word('design.n.eng')
+exwn.words('design')
+-> [Word('design.n.eng'),Word('design.v.eng')]
+
+# iterator
+for word in exwn.all_words(pos=[pos], lang=[lang]):
+  ...
+
+word = exwn.word('design','n') (=> Word('design.n.eng'))
+
+word.name()
+->'design'
+
+word.pos()
+->'n'
+
+word.lang()
+->'eng'
+
+word.synsets()
+->[Synset('design.n.01'), Synset('design.n.02'), Synset('blueprint.n.01'), Synset('design.n.04'), Synset('purpose.n.01'), Synset('design.n.06'), Synset('invention.n.01')]
+
+word.lemmas()
+->[Lemma('design.n.01.design'), Lemma('design.n.02.design'), Lemma('blueprint.n.01.design'), Lemma('design.n.04.design'), Lemma('purpose.n.01.design'), Lemma('design.n.06.design'), Lemma('invention.n.01.design')]
+
+word.vector(topic=topic)
+->[  1.10705128e-05   1.98191884e-05   1.39555417e-05   9.93190906e-07
+  -2.22414645e-05   1.99030851e-05   8.21694933e-06  
+
+  ...
+
+   3.69272634e-08  -8.30829140e-06  -2.96530259e-06  -2.12390994e-05]
+
+word.ambiguity(topic='automotive')
+->1.12806609165
+
+word.topic_relatedness(topic='automotive')
+->0.329674593626
+
+other = exwn.word('fun','n')
+
+word.association(other, topic='automotive')
+->(0.26641401556350547, 4) % (relatedness, shortest path distance)
+
+########################################
+# new feature exWordNet
+########################################
+obj = Synset('design.n.01')
+lemma = Lemma('design.n.01.design')
+
+exwn.vector(obj)
+->[ -7.66520000e-02  -6.46370000e-02  -4.26910000e-02   1.31290000e-02
+   9.02590000e-02   2.69600000e-03   3.52740000e-02  
+
+   ...
+
+   2.47840000e-02  -1.24070000e-02  -4.05540000e-02   1.01210000e-01]
+
+exwn.lemma_freq(lemma)
+->2.414572427514536e-06
+
+exwn.topic_vector('automotive')
+->[  1.21331815e-03   6.99298846e-03  -4.08518871e-03   9.87208609e-03
+  -5.81681535e-03   2.28192701e-03   3.73350873e-03  
+
+  ...
+
+   7.80417654e-04   2.82445540e-03   5.24175109e-03  -1.18144666e-03]
+
+exwn.relatedness(obj, lemma)
+->-0.128979924671
+```
---
+++ exWordNet.py
@@ -0,0 +1,322 @@
+from nltk.corpus import wordnet as wn
+from nltk.util import binary_search_file
+import numpy as np
+
+class exWordNetError(Exception):
+    """An exception class for wordnet-related errors."""
+
+def _relatedness(v_in, v_out):
+    return sum(v_in*v_out)/np.sqrt(sum(v_in*v_in)*sum(v_out*v_out))
+
+class Word(object):
+    # used in ambiguity which is for weighting function
+    _DELTA = 0.65
+    # used in association which is weight for shortest path shortest path distance
+    # the bigger k is, the more specific associations may be appear
+    _k = 0.5
+
+    def __init__(self, exwordnet, name, pos, lang='eng'):
+        self._exwordnet = exwordnet
+        if name not in wn.all_lemma_names(pos=pos, lang=lang):
+            raise exWordNetError('word %s.%s in %s is not defined in WordNet' % (name, pos, lang))
+
+        self._name = name
+        self._pos = pos
+        self._lang = lang
+        self._key = '%s.%s.%s' % (name, pos, lang)
+        # self._vector = self.exwordnet._vector(self)
+
+    def name(self):
+        return self._name
+
+    def pos(self):
+        return self._pos
+
+    def lang(self):
+        return self._lang
+
+    def synsets(self):
+        return self._exwordnet.synsets(self._name, pos=self._pos, lang=self._lang)
+
+    def lemmas(self):
+        return self._exwordnet.lemmas(self._name, pos=self._pos, lang=self._lang)
+
+    ###############################
+    # Calc vector for the topic
+    ###############################
+    def vector(self, topic=None):
+        if topic == None:
+            vector = self._exwordnet.vector(self)
+        else:
+            vector = np.zeros(300)
+            for l in self.lemmas():
+                try:
+                    vector += self._exwordnet.lemma_freq(l, topic)*self._exwordnet.vector(l)
+                except exWordNetError:
+                    continue
+
+        if sum(vector*vector)==0:
+            raise exWordNetError('vector for %r in %r is not properly calculated' % (self, topic))
+
+        return vector
+
+    ###############################
+    # Calc AMBIGUITY
+    ###############################
+    def ambiguity(self, topic='general'):
+        f = []
+        for l in self.lemmas():
+            f.append(self._exwordnet.lemma_freq(l, topic))
+        try:
+            f = np.array(f)/max(f)
+            ambiguity = sum([np.power(v, self._DELTA)/(np.power(np.power(v, self._DELTA)+np.power((1-v), self._DELTA),1/self._DELTA)) for v in f])
+            return ambiguity
+        except ZeroDivisionError:
+            raise exWordNetError('all frequency are 0 for %r' % self)
+
+    ###############################
+    # Calc TOPIC RELATEDNESS
+    ###############################
+    def topic_relatedness(self, topic):
+        tv = self._exwordnet.topic_vector(topic)
+        v = self.vector(topic=topic)
+        return _relatedness(v, tv)
+
+    ###############################
+    # Calc ASSOCIATION
+    ###############################
+    def association(self, other, topic=None, index=False):
+        rel = _relatedness(self.vector(topic=topic), other.vector(topic=topic))
+        sps = []
+        for s1 in self.synsets():
+            for s2 in other.synsets():
+                sp = s1.shortest_path_distance(s2)
+                sps.append(sp)
+
+        if len(sps)==0:
+            raise exWordNetError('%r and %r are not connected' % (self, other))
+
+        sp = min(sps)
+
+        # return calculated index or just relatedness and shortest path distance
+        if index:
+            return rel*np.log(1+sp/self._k)
+        else:
+            return rel, sp
+
+
+    def _related(self, relation_symbol=None):
+        rws = []
+        if relation_symbol == None:
+            for s in self.synsets():
+                for l in s.lemmas(lang=self._lang):
+                    if l.name()!=self._name:
+                        rws.append(l.name())
+        else:
+            for s in self.synsets():
+                for ss in s._related(relation_symbol):
+                    for l in ss.lemmas(lang=self._lang):
+                        if l.name()!=self._name:
+                            rws.append(l.name())
+
+        return sorted(list(map(lambda x: Word(self._exwordnet, x, self._pos, lang=self._lang), list(set(rws)))))
+
+    def __repr__(self):
+        tup = type(self).__name__, self._key
+        return "%s('%s')" % tup
+
+    def __hash__(self):
+        return hash(self._name)
+
+    def __eq__(self, other):
+        return self._key == other._key
+
+    def __ne__(self, other):
+        return self._key != other._key
+
+    def __lt__(self, other):
+        return self._key < other._key
+
+class exWordNet(object):
+    """
+    **FIRST, DEFINE ROOT FOR VECTOR and FREQ DATA FOLDER**
+
+    extention of WordNet
+    You can use WordNet method as usual WordNet
+
+    following new method added
+    * word(name, pos, lang)
+      construct word object by name, pos, lang
+    * words(name, lang)
+      find possible words object by name, lang
+    * all_words()
+      iterator for all word objects which can be limited by pos
+    * vector(obj)
+      find vector for the object
+    * lemma_freq(lemma, topic)
+      find frequency for the lemma in the topic
+    * topic_vector(topic)
+      find vector for the topic
+
+    """
+    _TOPICS = ['general', 'automotive', 'fashion', 'music']
+
+    def __init__(self, root):
+        self._root = root
+
+    def topics(self):
+        return self._TOPICS
+
+    def word(self, name, pos, lang='eng'):
+        word = Word(self, name, pos, lang=lang)
+        return word
+
+    def lemma(self, name, lang='eng'):
+        lemma = wn.lemma(name, lang=lang)
+        # lemma._vector = self._vector(lemma)
+        # lemma._freqs = {}
+        # for t in self._TOPICS:
+        #     lemma._freqs[t] = self._lemma_freq(lemma, t)
+        return lemma
+
+    def synset(self, name):
+        synset = wn.synset(name)
+        # synset._vector = self._vector(synset)
+        return synset
+
+    def words(self, name, lang='eng'):
+        poss = []
+        words = []
+        for s in wn.synsets(name, lang=lang):
+            poss.append(s.pos())
+        for pos in list(set(poss)):
+            word = Word(self, name, pos, lang=lang)
+            words.append(word)
+        return sorted(words)
+
+    def synsets(self, lemma, pos=None, lang='eng'):
+        synsets = wn.synsets(lemma, pos=pos, lang=lang)
+        # for s in synsets:
+        #     s._vector = self._vector(s)
+        return synsets
+
+    def lemmas(self, lemma, pos=None, lang='eng'):
+        lemmas = wn.lemmas(lemma, pos=pos, lang=lang)
+        # for l in lemmas:
+        #     l._vector = self._vector(l)
+        #     l._freqs = {}
+        #     for t in self._TOPICS:
+        #         l._freqs[t] = self._lemma_freq(l, t)
+        return lemmas
+
+    def all_synsets(self, pos=None):
+        for synset in wn.all_synsets(pos=pos):
+            # synset._vector = self._vector(synset)
+            yield synset
+
+    def all_words(self, pos=None, lang='eng'):
+        if pos is None:
+            pos_tags = ['n','v','a','r']
+        else:
+            pos_tags = [pos]
+
+        for pos_tag in pos_tags:
+            for l in wn.all_lemma_names(pos=pos_tag, lang=lang):
+                word = Word(self, l, pos_tag, lang=lang)
+                yield word
+
+    ###############################
+    # Load vector
+    ###############################
+    def vector(self, obj):
+        """
+        extract vector for the object
+        the object could be Word, Synset, Lemma
+        """
+        obj_name = type(obj).__name__.lower()
+        if obj_name == 'synset':
+            _data_file = open('%s/synsets.txt' % self._root, 'r')
+            line = binary_search_file(_data_file, obj._name)
+        elif obj_name == 'lemma':
+            tup = self._root, obj._lang
+            _data_file = open('%s/%s/lemmas.txt' % tup, 'r')
+            tup = obj._synset._name, obj._name
+            line = binary_search_file(_data_file, '%s:%s' % tup)
+        elif obj_name == 'word':
+            tup = self._root, obj._lang
+            _data_file = open('%s/%s/words.txt' % tup, 'r')
+            line = binary_search_file(_data_file, obj._name)
+        else:
+            raise exWordNetError('%r is not WordNet Object' % obj)
+
+        vector = self._vector_from_line(line)
+
+        if sum(vector*vector)==0:
+            raise exWordNetError('no vector for %r' % obj)
+
+        return vector
+
+    def _vector_from_line(self, line):
+        """
+        extract vector information from line
+        if the object is not found in vector file, or the vector for the object is not properly learned,
+        the returned vector would be zero vector
+        """
+        if line == None:
+            return np.zeros(300)
+        else:
+            vector = np.array(list(map(float, line.strip().split()[1:])))
+            return vector
+
+    ###############################
+    # Load frequency
+    ###############################
+    def lemma_freq(self, lemma, topic):
+        """
+        extract frequency of lemma in specific topic
+        if the object is not found in the file,
+        the returned frequency would be zero
+        """
+        # if wrong topic is entered, then raise error
+        if topic not in self._TOPICS:
+            raise exWordNetError('%s is not registered as a topic' % topic)
+
+        _data_file = open('%s/%s/freq/freq.%s.txt' % (self._root, lemma._lang, topic), 'r')
+        line = _data_file.readline()
+        total = int(line.split(' ')[0])
+        assert total > 0
+        tup = lemma._synset._name, lemma._name
+        line = binary_search_file(_data_file, '%s:%s' % tup)
+        if line == None:
+            return 0
+        else:
+            return int(line.strip().split(' ')[1])/total
+
+    ###############################
+    # Load topic vector
+    ###############################
+    def topic_vector(self, topic):
+        """
+        extract topic vector calculated as a frequency weighted lemma vector
+        """
+        if topic not in self._TOPICS:
+            raise exWordNetError('%s is not registered as a topic' % topic)
+
+        _data_file = open('%s/topics.txt' % self._root, 'r')
+
+        line = binary_search_file(_data_file, topic)
+        if line == None:
+            raise exWordNetError('no topic vector for %s' % topic)
+        else:
+            return self._vector_from_line(line)
+
+    ###############################
+    # Vector calculation method
+    ###############################
+    def relatedness(self, obj_in, obj_out):
+        vec_in = self.vector(obj_in)
+        vec_out = self.vector(obj_out)
+        return _relatedness(vec_in, vec_out)
+
+    def _relatedness(self, vec_in, vec_out):
+        return _relatedness(vec_in, vec_out)
